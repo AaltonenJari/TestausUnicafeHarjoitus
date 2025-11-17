2e4e4356a54707d9b4e14c25b90644699f3bbbf2
AaltonenJari <jaritapani.aaltonen@gmail.com>
2025-11-17 15:16:27 +0200

    Maksukortti-luokan testit

--- src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,50 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+
+    /*  Tee valmiiseen testiluokkaan MaksukorttiTest testit, jotka testaavat ainakin seuraavia asioita: */
+	@Test
+	public void kortinSaldoAlussaOikein() {
+	    assertEquals("saldo: 0.10",  kortti.toString());
+	}    
+
+	@Test
+	public void rahanLataaminenKasvattaaSaldoaOikein() {
+	    kortti.lataaRahaa(25);
+	    assertEquals("saldo: 0.35", kortti.toString());
+	}
+	
+	@Test
+	public void rahanOttaminenVahentaaSaldoaOikein() {
+	    kortti.otaRahaa(5);
+	    assertEquals("saldo: 0.5", kortti.toString());
+	}
+
+	@Test
+	public void SaldoEiMuutuRahaaEiTarpeeksi() {
+	    kortti.otaRahaa(20);
+	    assertEquals("saldo: 0.10",  kortti.toString());
+	}
+	
+    @Test
+    void otaRahaaPalauttaaTrueJosRahatRiittavat() {
+        boolean onnistui = kortti.otaRahaa(5);
+
+        // Testataan, että metodi palauttaa true
+        assertTrue(onnistui, "Metodin pitäisi palauttaa true, kun saldo riittää");
+
+        // Testataan, että saldo väheni oikein
+        assertEquals("saldo: 0.5", kortti.toString());
+    }
+
+    @Test
+    void otaRahaaPalauttaaFalseJosRahatEivatRiita() {
+        boolean onnistui = kortti.otaRahaa(20);
+
+        // Testataan, että metodi palauttaa false
+        assertFalse(onnistui, "Metodin pitäisi palauttaa false, kun saldo ei riitä");
+
+        // Testataan, että saldo ei muuttunut
+	    assertEquals("saldo: 0.10",  kortti.toString());
+    }
 }
